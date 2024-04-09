98d3bf235ef3de06f467ef562d0740d277a0ce86
leandroArraes <leandro.arraes.182@gmail.com>
2024-04-09 22:42:41 +0000

    foi

---
+++ src/produto/produto.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put} from '@nestjs/common';
+import { ProdutoService } from './produto.service';
+import { CreateProdutoDto } from './dto/create-produto.dto';
+import { UpdateProdutoDto } from './dto/update-produto.dto';
+
+@Controller('produto')
+export class ProdutoController {
+  constructor(private readonly produtoService: ProdutoService) {}
+
+  @Post()
+  create(@Body() createProdutoDto: CreateProdutoDto) {
+    return this.produtoService.create(createProdutoDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.produtoService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.produtoService.findOne(+id);
+  }
+
+  @Put(':id')
+  async update(@Body() body, @Param() params) {
+    return {
+      method: 'put',
+      body,
+      params
+    };
+  }
+
+  @Patch(':id')
+  updateSelect(@Param('id') id: string, @Body() updateProdutoDto: UpdateProdutoDto) {
+    return this.produtoService.update(+id, updateProdutoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.produtoService.remove(+id);
+  }
+}
